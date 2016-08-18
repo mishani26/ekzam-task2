4d847124824d44d54de874a18dcc74acdc58f355
Micheal <misha.badil@gmail.com>
2016-08-18 19:26:48 +0300

    it must work

--- src/node.js
+++ src/node.js
@@ -1,20 +1,39 @@
 class Node {
 	constructor(data, priority) {
-     parent: 0,
-     left: parent*2+1,
-     right: left+1;
+		this.data=data;
+		this.priority=priority;
+		this.parent=null;
+		this.left=null;
+		this.right=null;
 	}
 
 	appendChild(node) {
-
+		if (this.left==null) {
+			this.left=node;
+		}
+		else if	(this.right==null) {
+			this.right=node;
+		}	
 	}
 
 	removeChild(node) {
-
+		if (this.left==node) {
+			this.left=null;
+		}
+		else if	(this.right==node) {
+			this.right=null;
+		}
+		else
+			throw "ERROR";	
 	}
 
 	remove() {
-
+		if (this.right!=null) {
+			this.data.removeChild(this.right);
+		}
+		else if (this.left!=null) {
+			this.data.removeChild(this.left);
+		} 
 	}
 
 	swapWithParent() {
